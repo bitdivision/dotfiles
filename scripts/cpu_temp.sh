1f4b2ffb3b8f39b20cef552bcdb58cf6d6e856c2
Richard Fortescue-Webb <rfwebb@gmail.com>
2016-10-30 00:00:18 +0100

    Host specific updates

--- scripts/cpu_temp.sh
+++ scripts/cpu_temp.sh
@@ -2,4 +2,4 @@
 #sensors | grep "Physical id" | tail -n 1 | cut -d "+" -f2 | cut -d "(" -f1 | cut -d "." -f1 | awk '{$1=$1};1' | rev | cut -c 4- | rev
 
 # Gets the physical id temp from sensors and then strip stuff to just a number
-sensors | grep "Physical id" | cut -d "+" -f2 | cut -d "(" -f1 | cut -d "." -f1 | awk '{$1=$1};1'
+sensors | grep "Core 0" | cut -d "+" -f2 | cut -d "(" -f1 | cut -d "." -f1 | awk '{$1=$1};1'
