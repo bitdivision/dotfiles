3792fa1bc4a11b23a8452b4e069d96677baec906
Richard Fortescue-Webb <rfwebb@gmail.com>
2016-12-10 13:47:04 +0000

    Conky i3 updates

--- scripts/i3-startup.sh
+++ scripts/i3-startup.sh
@@ -10,7 +10,5 @@
 #i3-msg exec urxvt -e "irssi --connect=irc.freenode.net --nick=naminator"
 #Problem is that there is no way to split irssi vertically so we have to use seperate windows really. This means that we need a way to connect to different servers on startup solely using the terminal and not within irssi. There are ways to connect automatically to a channel when the --connect flag is used but this means only 1 channel per server per window. Also channels are hidden when connected to on startup.
 
-#Start chromium on #2 and focus
 i3-msg workspace 1
-i3-msg exec airvpn
 
