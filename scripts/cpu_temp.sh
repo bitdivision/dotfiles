5da360be9ab0baa054809acff02839673131aee6
Richard Fortescue-Webb <rfwebb@gmail.com>
2019-09-29 12:47:21 +0100

    Fix cpu and tmux themes

--- scripts/cpu_temp.sh
+++ scripts/cpu_temp.sh
@@ -1,7 +1,13 @@
 # If you want the decimal place
 #sensors | grep "Physical id" | tail -n 1 | cut -d "+" -f2 | cut -d "(" -f1 | cut -d "." -f1 | awk '{$1=$1};1' | rev | cut -c 4- | rev
 
-# Gets the physical id temp from sensors and then strip stuff to just a number
-sensors | grep "Core 0" | cut -d "+" -f2 | cut -d "(" -f1 | cut -d "." -f1 | awk '{$1=$1};1'
-# Ryzen doesn't show up as a core on lm_sensors yet
-#sensors | grep "Tdie" | cut -d "+" -f2 | cut -d "(" -f1 | cut -d "." -f1 | awk '{$1=$1};1'
+intel_temp=$( sensors | grep "Core 0" )
+ryzen_temp=$( sensors | grep "Tdie" )
+
+if [ -n "$intel_temp" ]; then
+    # Gets the physical id temp from sensors and then strip stuff to just a number
+    echo $intel_temp | cut -d "+" -f2 | cut -d "(" -f1 | cut -d "." -f1 | awk '{$1=$1};1'
+else
+    # Ryzen doesn't show up as a core on lm_sensors yet
+    echo $ryzen_temp | cut -d "+" -f2 | cut -d "(" -f1 | cut -d "." -f1 | awk '{$1=$1};1'
+fi
